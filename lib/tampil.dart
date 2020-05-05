91393158e07c2c79759b535dd359083b4e5cafd9
rachmadona48 <rayci232@gmail.com>
2020-05-05 18:40:02 +0700

    upload awal

---
+++ lib/tampil.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebViewInFlutter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: 'http://120.89.89.225:8071',
+      // url: 'https://kucingtekno.com',
+      // hidden: true,
+      // appBar: AppBar(title: Text("Kucing Tekno Com ")),
+    );
+  }
+}
